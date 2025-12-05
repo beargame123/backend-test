8cf47120e7abf413deb42496ee7a96ac00e313f0
beargame <hoyoung7827@gmail.com>
2025-12-05 15:09:47 +0900

    basePackages 지정

--- user-service/src/main/kotlin/server/project/global/config/MongoConfig.kt
+++ user-service/src/main/kotlin/server/project/global/config/MongoConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.MongoTransactionManager
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @Configuration
-@EnableMongoRepositories
+@EnableMongoRepositories(basePackages = ["server.project"])
 class MongoConfig {
 
     @Bean
